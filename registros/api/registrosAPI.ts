eadcc5b2e3dc82965bbcd91039cbb7e316924974
Josué Celaya <127690403+polartic@users.noreply.github.com>
2024-12-09 01:41:59 -0700

    Delete registros directory

--- registros/api/registrosAPI.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const registrosAPI = axios.create({
-    baseURL: 'http://localhost:3001/api/registros'
-})
-
-export default registrosAPI;
--- registros/interfaces/registros-interface.ts
+++
@@ -1,8 +0,0 @@
-export interface Registro{
-    id_personal: number,
-    fecha: string,
-    hora: string,
-    movimiento:string,
-}
-
-export type RegistroAgregar = Registro
