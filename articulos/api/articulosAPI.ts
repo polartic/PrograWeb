12f2adeaeade5f19b11cb95485ab2182f2721313
Josué Celaya <127690403+polartic@users.noreply.github.com>
2024-12-09 01:41:05 -0700

    Delete articulos directory

--- articulos/api/articulosAPI.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const articulosAPI = axios.create({
-    baseURL: 'http://localhost:3001/api/articulos'
-})
-
-export default articulosAPI;
--- articulos/interfaces/articulos-interface.ts
+++
@@ -1,9 +0,0 @@
-export interface Articulo{
-    id: number,
-    descripcion: string,
-    precio: number,
-    cantidadA:number,
-    caducidad:string
-}
-
-export type ArticuloAgregar = Omit<Articulo,'id'>
