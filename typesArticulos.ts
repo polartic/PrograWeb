7852f52dd95fbc6ad7a3e1abffa9bb3d9bdea187
Josué Celaya <127690403+polartic@users.noreply.github.com>
2024-12-09 01:45:12 -0700

    Delete typesArticulos.ts

--- typesArticulos.ts
+++
@@ -1,8 +0,0 @@
-export interface Articulo {
-    id:number,
-    descripcion: String,
-    precio:number,
-    cantidadA:number,
-    caducidad:string
-}
-export type ArticuloNuevo = Omit<Articulo,'id'>
