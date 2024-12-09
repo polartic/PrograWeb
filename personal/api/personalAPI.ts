e5fe1d17575b390f97fd4854309e4200b0ba4a54
Josué Celaya <127690403+polartic@users.noreply.github.com>
2024-12-09 01:41:43 -0700

    Delete personal directory

--- personal/api/personalAPI.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const personalAPI = axios.create({
-    baseURL: 'http://localhost:3001/api/personal'
-})
-
-export default personalAPI;
--- personal/interfaces/personal-interface.ts
+++
@@ -1,9 +0,0 @@
-export interface Personal{
-    id: number,
-    nombre: string,
-    direccion: string,
-    telefono:string,
-    estatus:number
-}
-
-export type PersonalAgregar = Omit<Personal,'id'>
