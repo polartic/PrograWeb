5bc7cf91578b7ac2c671a6bd5a69a48beb6ac688
Josué Celaya <127690403+polartic@users.noreply.github.com>
2024-12-09 01:45:18 -0700

    Delete typesClientes.ts

--- typesClientes.ts
+++
@@ -1,9 +0,0 @@
-export interface Cliente {
-    id:number,
-    nombre: String,
-    direccion:String,
-    telefono:String,
-    correo:string,
-    ciudad:string
-}
-export type ClienteNuevo = Omit<Cliente,'id'>
